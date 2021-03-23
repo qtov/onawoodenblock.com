7ff1eb3dbc02f340d19f1c295598e2f4e2754d28
Paul Mihai FORGACI <pforgaci@bitdefender.com>
2021-03-24 00:09:27 +0200

    meow

--- pelicanconf.py
+++ pelicanconf.py
@@ -31,4 +31,7 @@ SOCIAL = (('You can add links in your config file', '#'),
 DEFAULT_PAGINATION = 15
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+# RELATIVE_URLS = True
+
+STATIC_PATHS = ['images', 'extra/CNAME']
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'}, }
